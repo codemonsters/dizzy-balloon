23a6f5573e32308223d1874492834e639cf91835
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2019-11-22 20:02:23 +0100

    Sigo con animLoader

--- animationLoader.lua
+++ animationLoader.lua
@@ -23,54 +23,61 @@ animacionTestJugador = {
     pararse = {
         setParams = function(jugador) 
             jugador.left = false
-
         end,
         time = 0.5
     }
 }
 
-local ordered_keys = {}
+local animList = {}
+local orderedKeyList = {}
+local keyFrameList = {}
 
 function animLoader:applyAnim(target, anim)
-    self.target = target
+    numberAnims = table.getn(animList) + 1
+    anim.target = target
+    animList[numberAnims] = anim
     self.anim = anim
 
+    local ordered_keys = {}
     -- Se necesita ordenar la tabla de animacion para iterar en orden por ella
     for k in pairs(self.anim) do
         table.insert(ordered_keys, k)
     end
     
     table.sort(ordered_keys)
+    orderedKeyList[numberAnims] = ordered_keys
 
     self.counter = 0
     self.currFrame = 1
-    self:loadKeyFrame(self.anim, self.currFrame)
+    self:loadKeyFrame(self.anim, self.currFrame, numberAnims)
     self.currFrame = self.currFrame + 1
 end
 
 function animLoader:update(dt)
-    if self.anim then
-        self.counter = self.counter + dt
-        if self.counter >= self.keyFrame.time then
-            self:loadKeyFrame(self.anim, self.currFrame)
-
-            self.counter = 0
-            self.currFrame = self.currFrame + 1
+    for k, anim in pairs(animList) do
+        if anim then
+            self.counter = self.counter + dt
+            if self.counter >= keyFrameList[k].time then
+                self:loadKeyFrame(anim, self.currFrame, k)
+    
+                self.counter = 0
+                self.currFrame = self.currFrame + 1
+            end
         end
     end
 end
 
-function animLoader:loadKeyFrame(anim, index)
-    local k, v = ordered_keys[index], anim[ordered_keys[index]]
-    self.keyFrame = anim[k]
+function animLoader:loadKeyFrame(anim, index, numAnim)
+    local k, v = orderedKeyList[numAnim][index], anim[orderedKeyList[numAnim][index]]
+    keyFrameList[numAnim] = anim[k]
 
-    if not self.keyFrame then
+    if not keyFrameList[numAnim] then
         print("Se acabo la animación")
-        self.anim = nil
+        anim = nil
         return
     end
 
-    self.keyFrame.setParams(self.target)
+    keyFrameList[numAnim].setParams(anim.target)
 end
 
 return animLoader
