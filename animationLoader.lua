7ab51a5a6e41d68e989e67ec1d99c74c7465d5c6
pathMarker <oterolemabrais@gmail.com>
2019-11-29 19:25:52 +0100

    animationLoader puede ejecutar animaciones en paralelo

--- animationLoader.lua
+++ animationLoader.lua
@@ -1,83 +1,94 @@
 local animLoader = {}
 
 animacionTestJugador = {
-    frame1  = {
-        setParams = function(jugador)
-            jugador.right = true
-        end,
-        time = 1
+    keyFrames = {
+        frame1  = {
+            setParams = function(jugador)
+                jugador.right = true
+            end,
+            time = 1
+        },
+        frame2  = {
+            setParams = function(jugador) 
+                jugador.right = false
+            end,
+            time = 1
+        },
+        jump = {
+            setParams = function(jugador) 
+                jugador.left = true
+                jugador:jump()
+            end,
+            time = 0.5
+        },
+        pararse = {
+            setParams = function(jugador) 
+                jugador.left = false
+            end,
+            time = 0.5
+        }
     },
-    frame2  = {
-        setParams = function(jugador) 
-            jugador.right = false
-        end,
-        time = 1
-    },
-    jump = {
-        setParams = function(jugador) 
-            jugador.left = true
-            jugador:jump()
-        end,
-        time = 0.5
-    },
-    pararse = {
-        setParams = function(jugador) 
-            jugador.left = false
-        end,
-        time = 0.5
-    }
+    
+    orderedKeys = {}, -- claves de los keyframes ordenados
+
+    currFrame = nil, -- el keyframe que se está ejecutando
+    frameIndex = 1,
+    
+    target = nil -- el objeto animado
 }
 
 local animList = {}
-local orderedKeyList = {}
-local keyFrameList = {}
 
 function animLoader:applyAnim(target, anim)
-    numberAnims = table.getn(animList) + 1
     anim.target = target
-    animList[numberAnims] = anim
-    self.anim = anim
+    
+    table.insert(animList, anim)
+    anim.number = table.getn(animList)
 
-    local ordered_keys = {}
     -- Se necesita ordenar la tabla de animacion para iterar en orden por ella
-    for k in pairs(self.anim) do
-        table.insert(ordered_keys, k)
+    for k in pairs(anim.keyFrames) do
+        table.insert(anim.orderedKeys, k)
     end
     
-    table.sort(ordered_keys)
-    orderedKeyList[numberAnims] = ordered_keys
+    table.sort(anim.orderedKeys)
 
     self.counter = 0
-    self.currFrame = 1
-    self:loadKeyFrame(self.anim, self.currFrame, numberAnims)
-    self.currFrame = self.currFrame + 1
+    self:loadKeyFrame(anim, anim.frameIndex)
 end
 
 function animLoader:update(dt)
-    for k, anim in pairs(animList) do
+    for numAnim, anim in pairs(animList) do
         if anim then
             self.counter = self.counter + dt
-            if self.counter >= keyFrameList[k].time then
-                self:loadKeyFrame(anim, self.currFrame, k)
+            if self.counter >= anim.currFrame.time then
+                self:loadKeyFrame(anim, anim.frameIndex + 1)
     
                 self.counter = 0
-                self.currFrame = self.currFrame + 1
             end
         end
     end
 end
 
-function animLoader:loadKeyFrame(anim, index, numAnim)
-    local k, v = orderedKeyList[numAnim][index], anim[orderedKeyList[numAnim][index]]
-    keyFrameList[numAnim] = anim[k]
+function animLoader:loadKeyFrame(anim, index)
+    anim.frameIndex = index
 
-    if not keyFrameList[numAnim] then
+    local k, v = anim.orderedKeys[index], anim.keyFrames[anim.orderedKeys[index]]
+    anim.currFrame = anim.keyFrames[k]
+
+    if not anim.currFrame then
         print("Se acabo la animación")
-        anim = nil
-        return
+        table.remove(animList, findIndexInTable(animList, anim))
+    else
+        anim.currFrame.setParams(anim.target)
     end
+end
 
-    keyFrameList[numAnim].setParams(anim.target)
+function findIndexInTable(tabla,valor)
+    for index, value in pairs(tabla) do
+        if value == valor then
+            return index
+        end
+    end
 end
 
 return animLoader
