75c58398b7ccb216e7924fbb0d5a31d2284801ea
PabloHernandez0 <pablusky2004@hotmail.com>
2019-12-20 18:52:42 +0100

    Arreglada issue #31

--- animationLoader.lua
+++ animationLoader.lua
@@ -1,28 +1,28 @@
 local animLoader = {}
-
+-- No se puede poner nombres a los frames
 animacionTestJugador = {
     keyFrames = {
-        derecha1  = {
+        {
             setParams = function(jugador)
                 jugador.right = true
             end,
             time = 2
         },
-        izquierda1  = {
+        {
             setParams = function(jugador) 
                 jugador.right = false
                 jugador.left = true
             end,
             time = 1
         },
-        pararse = {
+        {
             setParams = function(jugador)
                 jugador.right = false 
                 jugador.left = false
             end,
             time = 0.5
         },
-        derecha2 = {
+        {
             setParams = function(jugador) 
                 jugador.left = false
                 jugador.right = true
@@ -30,7 +30,7 @@ animacionTestJugador = {
             time = 0.5
         },
 
-        jump = {
+        {
             setParams = function(jugador) 
                 jugador.right = false
                 jugador:jump()
@@ -39,8 +39,6 @@ animacionTestJugador = {
         },
 
     },
-    
-    orderedKeys = {}, -- claves de los keyframes ordenados
 
     currFrame = nil, -- el keyframe que se está ejecutando
     frameIndex = 1,
@@ -50,7 +48,7 @@ animacionTestJugador = {
 
 animacionTestEnemigo = {
     keyFrames = {
-        e_frame1  = {
+        {
             setParams = function(enemigo)
                 enemigo.velocidad_x = math.sqrt(8)
                 enemigo.velocidad_y = -math.sqrt(8)
@@ -58,22 +56,19 @@ animacionTestEnemigo = {
             time = 3.5
         },
 
-        e_frame2  = {
+        {
             setParams = function(enemigo)
                 enemigo.velocidad_x = -math.sqrt(8)
             end,
             time = 2.2
         },
-       e_frame3  = {
+        {
             setParams = function(enemigo)
                 enemigo.velocidad_y = -math.sqrt(8)
             end,
             time = 1
         },
-        
     },
-    
-    orderedKeys = {}, -- claves de los keyframes ordenados
 
     currFrame = nil, -- el keyframe que se está ejecutando
     frameIndex = 1,
@@ -89,13 +84,6 @@ function animLoader:applyAnim(target, anim)
     
     table.insert(animList, anim)
     anim.number = table.getn(animList)
-
-    -- Se necesita ordenar la tabla de animacion para iterar en orden por ella
-    for k in pairs(anim.keyFrames) do
-        table.insert(anim.orderedKeys, k)
-    end
-    
-    table.sort(anim.orderedKeys)
 
     anim.counter = 0
     self:loadKeyFrame(anim, anim.frameIndex)
@@ -117,8 +105,7 @@ end
 function animLoader:loadKeyFrame(anim, index)
     anim.frameIndex = index
 
-    local k, v = anim.orderedKeys[index], anim.keyFrames[anim.orderedKeys[index]]
-    anim.currFrame = anim.keyFrames[k]
+    anim.currFrame = anim.keyFrames[index]
     --print(k)
     if not anim.currFrame then
         table.remove(animList, findIndexInTable(animList, anim))
