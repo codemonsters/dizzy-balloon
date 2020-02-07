e4da8c55782b4aed9a22aef5846122b0040515d7
asextocm <a.sexto@codemonsters.es>
2020-02-07 21:12:32 +0100

    Refactor: movidos widgets de los menús a su propia tabla

--- screens/menus/mainMenu.lua
+++ screens/menus/mainMenu.lua
@@ -1,37 +1,49 @@
 local menu = {
     name = "Menú principal",
+    widgets = suit.new(ourTheme)
 }
 
 function menu.load()
     canvas = love.graphics.newCanvas(SCREEN_WIDTH, SCREEN_HEIGHT)
-    widgets = suit.new(ourTheme)
 end
 
 function menu.update(dt)
     love.graphics.setBlendMode("alpha")
 
-    widgets.layout:reset(SCREEN_WIDTH * 0.2, SCREEN_HEIGHT * 0.1)
-    widgets.layout:padding(0, SCREEN_WIDTH * 0.015)
+    menu.widgets.layout:reset(SCREEN_WIDTH * 0.2, SCREEN_HEIGHT * 0.1)
+    menu.widgets.layout:padding(0, SCREEN_WIDTH * 0.015)
     local mouseX, mouseY = love.mouse.getPosition()
     love.graphics.setFont(font_menu)
 
-    widgets:updateMouse((mouseX - desplazamientoX) / factorEscala, (mouseY - desplazamientoY) / factorEscala)
+    menu.widgets:updateMouse((mouseX - desplazamientoX) / factorEscala, (mouseY - desplazamientoY) / factorEscala)
 
-    widgets:Label("Dizzy Balloon", widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12))
+    menu.widgets:Label("Dizzy Balloon", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12))
 
     love.graphics.setFont(font_buttons)
 
-    if widgets:Button("Jugar",  widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
+    if menu.widgets:Button("Jugar", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
         music:stop()
         changeScreen(require("screens/game"))
     end
-    if widgets:Button("Preferencias", widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
+    if menu.widgets:Button("Preferencias", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
         changeScreen(require("screens/menus/preferencias"))
     end
-    if widgets:Button("Instrucciones", widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
+    if menu.widgets:Button("Instrucciones", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
         print("Te esperas. Todavía no está hecho. Si lo quieres usar, lo escribes y todos contentos :)")
     end
-    if widgets:Button("Salir", {color = {normal = {bg = {0, 0, 0, 0.15}, fg = {1, 1, 1}}, hovered = {fg = {1, 1, 1}, bg = {0.5, 0.5, 0.5, 0.5}}, active = {bg = {0, 0, 0, 0.5}, fg = {255, 255, 255}} }},  widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
+    if
+        menu.widgets:Button(
+            "Salir",
+            {
+                color = {
+                    normal = {bg = {0, 0, 0, 0.15}, fg = {1, 1, 1}},
+                    hovered = {fg = {1, 1, 1}, bg = {0.5, 0.5, 0.5, 0.5}},
+                    active = {bg = {0, 0, 0, 0.5}, fg = {255, 255, 255}}
+                }
+            },
+            menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)
+        ).hit
+     then
         os.exit()
     end
 end
@@ -40,14 +52,14 @@ function menu.draw()
     love.graphics.setCanvas(menu.canvas) -- canvas del HUD
     do
         love.graphics.setBlendMode("alpha")
-        widgets:draw()
+        menu.widgets:draw()
     end
 end
 
 function menu.keypressed(key, scancode, isrepeat)
     if key == "space" then
         music:stop()
-        changeScreen(require('screens/game'))
+        changeScreen(require("screens/game"))
     end
 end
 
--- screens/menus/preferencias.lua
+++ screens/menus/preferencias.lua
@@ -1,37 +1,46 @@
 local menu = {
     name = "Menú preferencias",
+    widgets = suit.new(ourTheme)
 }
 
 function menu.load()
     canvas = love.graphics.newCanvas(SCREEN_WIDTH, SCREEN_HEIGHT)
-    widgets = suit.new(ourTheme)
 end
 
 function menu.update(dt)
     love.graphics.setBlendMode("alpha")
 
-    widgets.layout:reset(SCREEN_WIDTH * 0.2, SCREEN_HEIGHT * 0.1)
-    widgets.layout:padding(0, SCREEN_WIDTH * 0.015)
+    menu.widgets.layout:reset(SCREEN_WIDTH * 0.2, SCREEN_HEIGHT * 0.1)
+    menu.widgets.layout:padding(0, SCREEN_WIDTH * 0.015)
     local mouseX, mouseY = love.mouse.getPosition()
     love.graphics.setFont(font_menu)
 
-    widgets:updateMouse((mouseX - desplazamientoX) / factorEscala, (mouseY - desplazamientoY) / factorEscala)
+    menu.widgets:updateMouse((mouseX - desplazamientoX) / factorEscala, (mouseY - desplazamientoY) / factorEscala)
 
-    widgets:Label("Preferencias", widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12))
+    menu.widgets:Label("Preferencias", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12))
 
     love.graphics.setFont(font_buttons)
 
-    if widgets:Button("Sonido",  widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
-
+    if menu.widgets:Button("Sonido", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
     end
-    if widgets:Button("Música", widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
-
+    if menu.widgets:Button("Música", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
     end
-    if widgets:Button("Idioma", widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
-
+    if menu.widgets:Button("Idioma", menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
     end
-    if widgets:Button("Volver", {color = {normal = {bg = {0, 0, 0, 0.15}, fg = {1, 1, 1}}, hovered = {fg = {1, 1, 1}, bg = {0.5, 0.5, 0.5, 0.5}}, active = {bg = {0, 0, 0, 0.5}, fg = {255, 255, 255}} }},  widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)).hit then
-        changeScreen(require('screens/menu'))
+    if
+        menu.widgets:Button(
+            "Volver",
+            {
+                color = {
+                    normal = {bg = {0, 0, 0, 0.15}, fg = {1, 1, 1}},
+                    hovered = {fg = {1, 1, 1}, bg = {0.5, 0.5, 0.5, 0.5}},
+                    active = {bg = {0, 0, 0, 0.5}, fg = {255, 255, 255}}
+                }
+            },
+            menu.widgets.layout:row(SCREEN_WIDTH * .6, SCREEN_HEIGHT * 0.12)
+        ).hit
+     then
+        changeScreen(require("screens/menu"))
     end
 end
 
@@ -39,13 +48,13 @@ function menu.draw()
     love.graphics.setCanvas(menu.canvas) -- canvas del HUD
     do
         love.graphics.setBlendMode("alpha")
-        widgets:draw()
+        menu.widgets:draw()
     end
 end
 
 function menu.keypressed(key, scancode, isrepeat)
     if key == "space" then
-        changeScreen(require('screens/menu'))
+        changeScreen(require("screens/menu"))
     end
 end
 
