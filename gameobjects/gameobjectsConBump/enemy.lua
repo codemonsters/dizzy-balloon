c9bc9ab744dfb3ab2b920a8e017d4cbe4fd91192
Braisa <oterolemabrais@gmail.com>
2020-02-21 19:10:19 +0100

    Casi acabado enemy.lua Falta meterlo en game.lua

--- gameobjects/gameobjectsConBump/enemy.lua
+++ gameobjects/gameobjectsConBump/enemy.lua
@@ -11,39 +11,35 @@ local Enemy = {
     vy = 0,
     isEnemy = true,
     image = love.graphics.newImage("assets/image/old/enemy.png"),
-    enemyFilter = function(item, other)
-        if     other.isPlayer then return "slide"
-        elseif other.isBomb   then return "slide"
-        else                       return "bounce"
-        end
-    end,
     states = {
         moving = {
+            collisionFilter = function(item, other)
+                if other.isBlock then
+                    return "bounce"
+                elseif other.isSeed then
+                    return "bounce"
+                elseif other.isEnemy then
+                    return "bounce"
+                elseif other.isMushroom then
+                    return "bounce"
+                elseif other.isLimit then
+                    return "bounce"
+                elseif other.isPlayer then
+                    return "touch"
+                end
+            end,
             load = function(self)
-                self.vx = math.cos(self.direction) * speed
-                self.vy = math.sin(self.direction) * speed
+                self.vx = math.cos(self.direction) * self.speed
+                self.vy = math.sin(self.direction) * self.speed
             end,
             update = function(self, dt)
-                self.x, self.y, cols, len = self.world:move(self, self.movSigx, self.movSigy, self.enemyFilter)
-                if len > 0 then
-                    local col = cols[1]
-                    if col.other.isBlock or col.other.isSeed or col.other.isEnemy or col.other.isMushroom then
-                        vecBounce = {x = col.bounce.x - col.touch.x, y = col.bounce.y - col.touch.y}
-
-                        moduloBounce = math.sqrt(math.pow(vecBounce.x, 2) + math.pow(vecBounce.y, 2))
-                        vectorUnitario = {x = vecBounce.x / moduloBounce, y = vecBounce.y / moduloBounce}
-
-                        self.velocidad_x = vectorUnitario.x * self.moduloVelocidad
-                        self.velocidad_y = vectorUnitario.y * self.moduloVelocidad
-
-                        seno = self.velocidad_y / self.moduloVelocidad -- calculo del ángulo con relación a la vertical tras el choque
-                        anguloEnGradosConVertical = math.asin(seno) * 360 / (2 * math.pi)
-
-                        if math.abs(anguloEnGradosConVertical) <= 15 then
-                            self:change_state(self.states.swiping)
-                        end
-                    elseif col.other.isPlayer then
-                        col.other:empujar({x = self.vx * 2, y = self.vy * 2}, self)
+                local goalX, goalY = self.x + self.vx * dt, self.y + self.vy * dt
+                local actualX, actualY, cols, len = self.world:move(self, goalX, goalY, self.state.collisionFilter)
+                self.x, self.y = actualX, actualY
+                for i = 1, len do
+                    local other = cols[i].other
+                    if other.isPlayer then
+                        other:empujar({x = self.vx * 2, y = self.vy * 2}, self)
                     end
                 end
             end,
@@ -51,51 +47,70 @@ local Enemy = {
             end
         },
         swiping = {
+            collisionFilter = function(item, other)
+                if true then
+                    return "slide"
+                end
+            end,
             load = function(self)
-                self.initalVel = self.moduloVelocidad
+                self.startingSpeed = self.speed
                 self.horizontal = true
-                self.lastXvelocity = self.initalVel
-                self.yAfterDiving = 0
-                self.upBounceCounter = 0
+                self.divingStartingY = 0
+                self.bounceCounter = 0
                 -- cuenta los choques cuando el enemigo no puede seguir bajando
-                self.velocidad_x = self.initalVel
-                self.velocidad_y = 0
+                self.vx = self.speed
+                self.vy = 0
             end,
             update = function(self, dt)
-                self.x, self.y, cols, len = self.world:move(self, self.movSigx, self.movSigy, self.enemyFilter)
+                local goalX, goalY = self.x + self.vx * dt, self.y + self.vy * dt
+                local actualX, actualY, cols, len = self.world:move(self, goalX, goalY, self.state.collisionFilter)
+                self.x, self.y = actualX, actualY
 
                 if len > 0 then
-                    local col = cols[1]
-                    if not col.other.isBomb and not col.other.isPlayer then --col.other.isBlock or col.other.isSeed or col.other.isEnemy or col.other.isMushroom then
-                        if self.horizontal then -- colisión yendo hacia arriba
-                            self.horizontal = false
-                            self.yAfterDiving = self.y
-                            self.lastXvelocity = self.velocidad_x
-                            self.velocidad_x = 0
-                            self.velocidad_y = 4
-                        else -- colisión llendo hacia abajo
-                            self.upBounceCounter = self.upBounceCounter + 1
-                            self.horizontal = true
-                            self.velocidad_x = -self.lastXvelocity
-                            self.velocidad_y = 0
+                    local other = cols[i].other
+                    if self.horizontal then
+                        for i = 1, len do
+                            if other.isBlock or other.isMushroom or other.isSeed then
+                                self.speed = self.speed * 1.25
+                                local checkX, checkY, checkCols, checkLen = self.world:queryRect(self.x, self.y, self.x + self.width, self.y + self.height * 2, self.state.collisionFilter)
+                                if checkLen == 0 then
+                                    self.horizontal = false
+                                    self.divingStartingY = self.y
+                                    self.vx = 0
+                                    self.vy = self.speed
+                                end
+                            elseif other.isPlayer then
+                                other:empujar({x = self.vx * 2, y = 0}, self)
+                            end
                         end
-                    elseif col.other.isPlayer then
-                        col.other:empujar({x = self.velocidad_x * 2, y = self.velocidad_y * 2}, self)
+                    else
+                        for i = 1, len do
+                            if other.isBlock or other.isMushroom or other.isSeed then
+                                self.speed = self.speed * 1.25
+                                self.horizontal = true
+                                self.divingStartingY = 0
+                                self.vx = self.speed
+                                self.vy = 0
+                                self.bounceCounter = self.bounceCounter + 1
+                            elseif other.isPlayer then
+                                other:empujar({x = 0, y = self.vy * 2}, self)
+                            end
+                        end
+                        if self.y - self.divingStartingY >= self.height * 2 then
+                            self.horizontal = true
+                            self.divingStartingY = 0
+                            self.vx = self.speed
+                            self.vy = 0
+                            self.bounceCounter = 0
+                        end
                     end
-                end
-
-                if not self.horizontal then
-                    if math.abs(self.yAfterDiving - self.y) >= self.height then
-                        self.upBounceCounter = 0 -- reseteamos si el enemigo es capaz de bajar
-                        self.horizontal = true
-                        self.velocidad_x = -self.lastXvelocity
-                        self.velocidad_y = 0
+                    
+                    if self.bounceCounter >= 3 then
+                        self.direction = 45
+                        self.speed = self.startingSpeed
+                        self.vx, self.vy = self.speed, self.speed
+                        self:changeState(self.states.moving)
                     end
-                end
-
-                if self.upBounceCounter == 3 then
-                    self.direction = 45
-                    self:change_state(self.states.moving)
                 end
             end,
             draw = function(self, dt)
@@ -113,3 +128,70 @@ local Enemy = {
 }
 
 Enemy.__index = Enemy
+
+function Enemy.new(name, x, y, world, game, direction)
+    local enemy = {}
+    enemy.riders = {}
+    enemy.name = name
+    enemy.game = game
+    enemy.world = world
+    enemy.dead = false
+    enemy.direction = direction
+    enemy.x = x
+    enemy.y = y
+    enemy.world:add(enemy, enemy.x, enemy.y, Enemy.width, Enemy.height)
+    setmetatable(enemy, Enemy)
+    enemy:changeState(enemy.states.moving)
+    return enemy
+end
+
+function Enemy:update(dt)
+    if self.dead then
+        self.game.remove_enemy(self)
+        return
+    end
+
+    self.state.update(self, dt)
+
+    if table.getn(self.riders) > 0 then
+        for key, rider in ipairs(self.riders) do
+            if not rider.montado then -- eliminar de la tabla riders los que se desmonten
+                table.remove(self.riders, key)
+            else
+                vector = {x = self.velocidad_x, y = self.velocidad_y}
+                rider:empujar(vector, self)
+            end
+        end
+    end
+end
+
+function Enemy:draw()
+    if self.dead then
+        return
+    end
+    love.graphics.draw(
+        self.image,
+        self.x,
+        self.y,
+        0,
+        self.width / self.image:getWidth(),
+        self.height / self.image:getHeight()
+    )
+end
+
+function Enemy:changeState(newState)
+    if self.state ~= newState then
+        self.state = newState
+        self.state.load(self)
+    end
+end
+
+function Enemy:montado(rider)
+    table.insert(self.riders, rider)
+end
+
+function Enemy:die()
+    self.dead = true
+end
+
+return Enemy
