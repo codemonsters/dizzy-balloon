f25021d96fe0493353384ad525904a393f89a411
pathMarker <pathmarkerer@gmail.com>
2020-02-21 19:54:59 +0100

    se mueve

--- gameobjects/airfly.lua
+++ gameobjects/airfly.lua
@@ -1,61 +1,69 @@
 local Enemy = require("gameobjects/enemy")
 
 local AirFlyClass = {}
+setmetatable(AirFlyClass, Enemy)
 AirFlyClass.__index = Enemy
 
 function AirFlyClass.new(name, x, y, world, game, direction)
-    local newObject = Enemy.new(name, x, y, world, game, direction)
+    local newObject = AirFlyClass.__index.new(name, x, y, world, game, direction)
     
     --setmetatable(enemy, Enemy)
     
     return newObject
 end
 
-AirFlyClass.flyFilter = function(item, other)
+AirFlyClass.__index.flyFilter = function(item, other)
     return "slide"
 end
 
-function AirFlyClass:dist(self, orig, dest)
+function AirFlyClass.__index:dist(orig, dest)
     return {x = dest.x - orig.x, y = dest.y - orig.y}
 end
 
-function AirFlyClass:mod(self, vec)
+function AirFlyClass.__index:mod(vec)
     return math.sqrt(math.pow(vec.x,2)+math.pow(vec.y,2))
+end
+
+function AirFlyClass.__index:setTarget(objetivo)
+    local dist = self:dist({x = self.x, y = self.y}, objetivo)
+
+    self.velocidad_x = dist.x/self:mod(dist) * self.moduloVelocidad
+    self.velocidad_y = dist.y/self:mod(dist) * self.moduloVelocidad
 end
 
 AirFlyClass.states.moving = {
     
     load = function(self)
-        self.objetivo = {x = WORLD_WIDTH/2, y = math.random(0, self.height * 10)}
-        dist = self:dist({x = self.x, y = self.y}, self.objetivo)
-        self.direction = math.asin(dist.y/self:mod(dist))
-        self.velocidad_x = math.cos(self.direction) * self.moduloVelocidad
-        self.velocidad_y = math.sin(self.direction) * self.moduloVelocidad
+        self.cargado = true
+        self.atacando = false
+        self.contadorDescanso = 0
+        self.objetivo = {x = math.random(0, WORLD_WIDTH), y = math.random(0, WORLD_HEIGHT/3)}
+        self:setTarget(self.objetivo)
     end,
     update = function(self, dt)
 
-        self.x, self.y, cols, len = self.world:move(self, self.movSigx, self.movSigy, self.flyFilter)
-
-        if len > 0 then
-            local col = cols[1]
-            if col.other.isBlock or col.other.isSeed or col.other.isEnemy or col.other.isMushroom then -- si se pone "if not other.col.isBomb" then da un error cuando el enemigo choca con algo, puede ser problema de la bomba no teniendo un collision_filter o algo por el estilo 
-                -- TODO: calculo de velocidad tras el choque
-                vecBounce = {x = col.bounce.x - col.touch.x, y = col.bounce.y - col.touch.y}
-                
-                moduloBounce = math.sqrt(math.pow(vecBounce.x, 2) + math.pow(vecBounce.y, 2))
-                vectorUnitario = {x = vecBounce.x / moduloBounce, y = vecBounce.y / moduloBounce}
-    
-                self.velocidad_x = vectorUnitario.x * self.moduloVelocidad
-                self.velocidad_y = vectorUnitario.y * self.moduloVelocidad
-                
-                seno = self.velocidad_y/self.moduloVelocidad -- calculo del ángulo con relación a la vertical tras el choque
-                anguloEnGradosConVertical = math.asin(seno) * 360/(2*math.pi)
-
-                if math.abs(anguloEnGradosConVertical) <= 15 then
-                    self:change_state(self.states.swiping)
+        self.contadorDescanso = self.contadorDescanso + dt
+        if self.contadorDescanso > 5 then
+            if self.contadorDescanso > 7 then
+                self.contadorDescanso = 0
+                self.cargado = true
+            end
+        else
+            self.x, self.y, cols, len = self.world:move(self, self.movSigx, self.movSigy, self.flyFilter)
+            distObj = self:mod(self:dist({x = self.x, y = self.y}, self.objetivo))
+            if distObj < 10 or len > 0 then
+                if distObj < 10 then
+                    if self.atacando then
+                        self.atacando = false
+                    end
                 end
-            elseif col.other.isPlayer then
-                col.other:empujar({x = self.velocidad_x*2, y = self.velocidad_y*2}, self);
+                self.objetivo = {x = math.random(0, WORLD_WIDTH), y = math.random(0, WORLD_HEIGHT/3)}
+                if math.random(0, 100) < 50 and self.cargado then
+                    self.objetivo.x = self.game.currentLevel.player.x
+                    self.atacando = true
+                end
+
+                self:setTarget(self.objetivo)
             end
         end
     end,
