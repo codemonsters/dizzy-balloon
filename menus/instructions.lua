4df3bc515c8ac6c7accd4348cba8361261f7d36c
PabloHernandez0 <49038740+PabloHernandez0@users.noreply.github.com>
2020-04-17 19:37:13 +0200

    Botón en instrucciones cambia entre instrucciones

--- menus/instructions.lua
+++ menus/instructions.lua
@@ -7,14 +7,44 @@ function menu.load(menuManager, screen)
     menu.menuManager = menuManager
     menu.screen = screen
     menu.musicOn = false
+    
+    tutorialStarted = false
+    tutorialPage = 1
+    tutorialPages = {
+        "Comenzar tutorial",
+        "Tu objetivo es llegar al cielo sin ser eliminado",
+        "Puedes moverte hacia los lados, saltar y lanzar bombas", 
+        "Puedes matar a los enemigos con bombas, y ellos pueden matarte a ti de aplastamiento",
+        "Si matas a un enemigo, la semilla que tenían encima caerá al suelo",
+        "Estas semillas pueden ser incubadas al posarse sobre ellas, y harán crecer un globo",
+        "Cuando te subas a estos globos, podrás conducirlos libremente por todo el nivel ", 
+        "Tu habilidad de lanzar bombas no estará disponible durante los viajes en globo", 
+        "Presta atención a las semillas que causan reacciones especiales al tocar el suelo",
+        "Las bombas que no hayan detonado correctamente se convertirán en setas", 
+        "Ten cuidado con la zona de explosión de tus propias bombas",  
+        "El espacio mínimo que necesitas en el techo para pasar al siguiente nivel es de tres semillas",
+        " Algunos niveles tienen plataformas a las que puedes subirte, y contra las que pueden aplastarte", 
+        "Pásalo bien",  
+        }
+
 end
 
 function menu.update(dt)
-    menu.widgets.layout:reset(SCREEN_WIDTH * 0.1, SCREEN_HEIGHT * 0.4)
+    menu.widgets.layout:reset(SCREEN_WIDTH * 0.1, SCREEN_HEIGHT * 0.1)
     menu.widgets.layout:padding(SCREEN_HEIGHT * 0.015, SCREEN_WIDTH * 0.015)
     local mouseX, mouseY = love.mouse.getPosition()
-
+    
     menu.widgets:updateMouse((mouseX - desplazamientoX) / factorEscala, (mouseY - desplazamientoY) / factorEscala)
+
+
+    if menu.widgets:Button(tutorialPages[tutorialPage], menu.widgets.layout:down(SCREEN_WIDTH * .8, SCREEN_HEIGHT * 0.4)).hit then
+       if tutorialPage < 14 then
+            tutorialPage = tutorialPage + 1
+       else
+            tutorialPage = 1
+       end
+    end
+
 
     if
         menu.widgets:Button(
