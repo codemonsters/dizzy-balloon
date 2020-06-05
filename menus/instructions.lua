5a5511c0e40c80222648530141534b13e2818f30
PabloHernandez0 <49038740+PabloHernandez0@users.noreply.github.com>
2020-06-05 19:15:50 +0200

    Añadidos strings tutorial

--- menus/instructions.lua
+++ menus/instructions.lua
@@ -9,7 +9,54 @@ function menu.load(menuManager, screen)
     -- creamos los botones del menú
     local strings = require("misc/strings")
     local widgetsClass = require("misc/widgets")
+    local lang = config.get("language")
+    local nPaginaTutorial = 1
+
+    if lang == "es" then
+        Tstrings = {
+            "Tu objetivo es ir superando niveles hasta llegar al cielo",
+            "Puedes moverte, saltar y lanzar bombas",
+            "Puedes matar a los enemigos con bombas, y ellos pueden matarte aplastándote",
+            "Si matas a un enemigo, la semilla que había sobre él caerá al suelo",
+            "Súbete a una semilla para incubarla y que crezca un globo",
+            "Pásalo bien", 
+        }
+    elseif lang == "en" then
+        Tstrings = {
+            "Your goal is to beat levels until you reach the sky",
+            "You can move, jump and throw bombs",
+            "You can kill enemies with bombs, and they can kill you by crushing you",
+            "If you kill an enemy, the seed that was over him will fall to the floor",
+            "These seeds can be hatched to grow a balloon",
+            "Have fun", 
+        }
+    end
+
+    print(Tstrings[nPaginaTutorial])
     local buttons = {
+        {
+            --local idioma = config.get("language"),
+            label = Tstrings[nPaginaTutorial],
+            callback = function()
+                print('hola')
+            end
+        },
+        {
+            label = getString(strings.previous),
+            callback = function()
+                if nPaginaTutorial > 1 then
+                    nPaginaTutorial = nPaginaTutorial - 1 
+                end
+            end
+        },
+        {
+            label = getString(strings.next),
+            callback = function()
+                if nPaginaTutorial < table.getn(Tstrings) then
+                    nPaginaTutorial = nPaginaTutorial + 1 
+                end
+            end
+        },
         {
             label = getString(strings.goBack),
             callback = function()
@@ -26,6 +73,7 @@ function menu.load(menuManager, screen)
 end
 
 function menu.update(dt)
+    print(nPaginaTutorial)
     for i = 1, #menu.widgets do
         menu.widgets[i].update()
     end
--- misc/strings.lua
+++ misc/strings.lua
@@ -43,9 +43,41 @@ local strings = {
         es = "Menú",
         en = "Menu"
     },
+    previous = {
+        es = "Anterior",
+        en = "Previous"
+    },
+    next = {
+        es = "Siguiente",
+        en = "Next"
+    },
     continue = {
         es = "Continuar",
         en = "Continue"
+    }, 
+    t1 = {
+        es = "Tu objetivo es llegar al cielo",
+        en = "Your goal is to beat levels until you reach the sky"
+    },
+    t2 = {
+        es = "Puedes moverte, saltar y lanzar bombas",
+        en = "You can move, jump and throw bombs"
+    },
+    t3 = {
+        es = "Puedes matar a los enemigos con bombas, y ellos pueden matarte aplastándote",
+        en = "You can kill enemies wtih bombs, and they can kill you by crushing you"
+    },
+    t4 = {
+        es = "Si matas a un enemigo, la semilla que había sobre él caerá al suelo",
+        en = "If you kill an enemy, the seed that was over him will fall to the floor"
+    },
+    t5 = {
+        es = "Súbete a una semilla para incubarla y que crezca un globo",
+        en = "Those seeds can be hatchec to grow a balloon"
+    },
+    t6 = {
+        es = "Pásalo bien",
+        en = "Have fun"
     }
 }
 
